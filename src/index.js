5968ed2570da49073903346877afae4aa825b82e
semdy <501859275@qq.com>
2018-12-14 12:01:29 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var PxToRpx = function (opts = {}) {
+  var def = {
+    filter: new RegExp('.(wxss|wxml|json)$')
+  }
+  this.setting = Object.assign({}, def, opts)
+}
+
+PxToRpx.prototype.apply = function apply(op) {
+  if (this.setting.filter.test(op.file)) {
+    if (op.code) {
+      op.code = op.code.replace(/\b(\d+(\.\d+)?)px\b/ig, function (match, $1) {
+        return $1 * 2 + 'rpx'
+      })
+    }
+  }
+  op.next()
+}
+
+moudle.export = PxToRpx
